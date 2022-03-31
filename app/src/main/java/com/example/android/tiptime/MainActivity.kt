2a0d99bf640bba01fdc85b11098911234cf6dd70
nikihdyt <nikihidayati20@gmail.com>
2022-03-31 13:08:41 +0700

    menghapus komentar tidak perlu

--- app/src/main/java/com/example/android/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/android/tiptime/MainActivity.kt
@@ -27,21 +27,6 @@ class MainActivity : AppCompatActivity() {
         // binding telah diterapkan.
         // sekarang, referensi terhadap View tidak harus menggunakan findViewById(), namun Objek binding secara otomatis menentukan referensi untuk setiap View dalam aplikasi Anda yang memiliki ID
 
-        // contoh :
-        // Old way with findViewById()
-        //   val myButton: Button = findViewById(R.id.my_button)
-        //   myButton.text = "A button"
-        // Better way with view binding
-        //   val myButton: Button = binding.myButton
-        //   myButton.text = "A button"
-        // Best way with view binding and no extra variable
-        //   binding.myButton.text = "A button"
-
-        // penamaan binding :
-        // Nama class binding dibuat dengan mengonversi nama file XML menjadi camel case dan menambahkan kata "Binding" ke bagian akhirnya.
-        // Demikian pula, referensi untuk setiap tampilan dibuat dengan menghapus garis bawah dan mengonversi nama tampilan menjadi camel case.
-        // Misalnya, activity_main.xml menjadi ActivityMainBinding, dan Anda dapat mengakses @id/text_view sebagai binding.textView.
-
         binding.calculateButton.setOnClickListener{ calculateTip() }
 
         // Set up key listener ke EditText agar menyembunyakan keyboard saat tombol "enter" ditekan
